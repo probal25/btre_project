b478147dfc8e7a92d2fd20629c0745a305eff5fa
probal25 <probal25@hotmail.com>
2019-09-27 02:39:30 +0600

    Initial Commit

--- btre/settings.py
+++ btre/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
+    'pages.apps.PagesConfig',
+    'listings',
+    'realtors',
+    'accounts',
+    'contacts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +62,7 @@ ROOT_URLCONF = 'btre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'tamplates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +83,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE'    : 'django.db.backends.postgresql',
+        'NAME'      : 'btredb',
+        'USER'      : 'postgres',
+        'PASSWORD'  : 'admin',
+        'HOST'      : 'localhost'
     }
 }
 
@@ -117,4 +128,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
+
+#media folder settings
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL   = '/media/'
+
+
+#Messages
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+#Email Config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '25probal@gmail.com'
+EMAIL_HOST_PASSWORD='probal199325'
+EMAIL_USE_TLS=True
